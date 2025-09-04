35428ec8c4c919d6d31c8633db1c68c8712ef6fc
Amit <amitsj@fss.co.in>
2025-09-04 15:23:15 +0530

    update nav component and home component

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'aj-home',
-  imports: [],
-  templateUrl: './home.html',
-  styleUrl: './home.css'
+    selector: 'aj-home',
+    styleUrl: './home.css',
+    templateUrl: './home.html',
+    imports: [
+        RouterLink
+    ]
 })
 export class Home {
+    protected readonly skills: WritableSignal<string[]> = signal([]);
+    protected readonly professionSummary: WritableSignal<string[]> = signal([]);
 
+    constructor () {
+        this.getProfessionSummary();
+
+        this.getSkills();
+    }
+
+    private getProfessionSummary(): void {
+        this.professionSummary.set(
+            [
+                'I have over 9 years of experience in software development. Experienced in designing scalable and high-performance applications across mobile, web, and backend technologies, leveraging Angular, Flutter, Node.js, Golang, Java Spring Boot, and NoSQL databases.',
+                'Developed a low code/no-code Angular platform that automates the generation of Java based microservices and UI applications, significantly enhancing development efficiency.',
+                'Expertise includes Clean Architecture, CQRS-based backend services in Golang, and dynamic caching mechanisms for WebSocket BFF services.',
+                'Lead development teams and collaborate with clients to deliver innovative solutions, optimize performance, and tackle complex engineering challenges.',
+            ]
+        );
+    }
+
+    private getSkills(): void {
+        this.skills.set(
+            [
+                'Angular',
+                'TypeScript',
+                'Flutter',
+                'Dart',
+                'Node.js',
+                'Express.js',
+                'Golang',
+                'Java',
+                'Spring Boot',
+                'MongoDB',
+                'Redis',
+                'Docker',
+                'Kubernetes',
+                'AWS',
+            ]
+        );
+    }
 }
