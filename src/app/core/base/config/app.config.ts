9e63b4e87157f260f4e68c11c23167623fd3bf71
Amit <amitsj@fss.co.in>
2025-09-07 11:00:28 +0530

    enabled hash location.

--- src/app/core/base/config/app.config.ts
+++ src/app/core/base/config/app.config.ts
@@ -1,13 +1,13 @@
 import { routes } from '../../../features/app/app.routes';
-import { provideRouter, withViewTransitions } from '@angular/router';
 import { withI18nSupport, provideClientHydration } from '@angular/platform-browser';
+import { provideRouter, withHashLocation, withViewTransitions } from '@angular/router';
 import { ApplicationConfig, provideZonelessChangeDetection, provideBrowserGlobalErrorListeners } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideBrowserGlobalErrorListeners(),
         provideZonelessChangeDetection(),
-        provideRouter(routes, withViewTransitions()),
+        provideRouter(routes, withViewTransitions(), withHashLocation()),
         provideClientHydration(withI18nSupport()),
     ]
 };
