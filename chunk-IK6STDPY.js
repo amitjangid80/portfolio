130f34929c4422e7cbbf2b15d8039674d9a61fa9
amitjangid80 <amitjangid80@users.noreply.github.com>
2025-09-07 07:27:48 +0000

    deploy: 72145d1c6906b18f0a2bc47068d3ab87219e3bc0

--- chunk-IK6STDPY.js
+++
@@ -1,1 +0,0 @@
-import{Na as o,Oa as a,Wa as c,ta as n}from"./chunk-EATPMOMW.js";import"./chunk-7CGTOI24.js";var s=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["aj-contact"]],decls:2,vars:0,template:function(t,m){t&1&&(o(0,"p"),c(1,"contact works!"),a())},encapsulation:2})};export{s as Contact};
---
+++ chunk-XFOY4AIW.js
@@ -0,0 +1,1 @@
+import{Na as s,Oa as n,Wa as r,ta as o}from"./chunk-YLP5W2AH.js";import"./chunk-7CGTOI24.js";var c=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["aj-projects"]],decls:2,vars:0,template:function(e,i){e&1&&(s(0,"p"),r(1,"projects works!"),n())},encapsulation:2})};export{c as Projects};
