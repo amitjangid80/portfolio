35bf208e930d3560392740f801109ddf2bd62e25
Amit <amitsj@fss.co.in>
2025-09-13 13:01:17 +0530

    added small screen support

--- src/app/shared/components/nav/nav.ts
+++ src/app/shared/components/nav/nav.ts
@@ -2,19 +2,24 @@ import { NavModel } from './common/nav.model';
 import { TextConstants } from '../../constants/text';
 import { NavConstants } from '../../constants/routes';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { signal, Component, WritableSignal } from '@angular/core';
+import { signal, Signal, viewChild, Component, ElementRef, WritableSignal } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
     selector: 'aj-nav',
     styleUrl: './nav.css',
     templateUrl: './nav.html',
     imports: [
+        NgClass,
         RouterLink,
         RouterLinkActive
     ]
 })
 export class Nav {
     protected readonly navList: WritableSignal<NavModel[]> = signal([]);
+    protected readonly showNavBar: WritableSignal<boolean> = signal(false);
+
+    protected readonly nav: Signal<ElementRef<HTMLElement> | undefined> = viewChild<ElementRef<HTMLElement>>("nav");
 
     constructor () {
         this.navList().push(
@@ -25,5 +30,9 @@ export class Nav {
                 { icon: 'pi-envelope', cssClass: 'nav-contact', title: TextConstants.contact, route: NavConstants.contact },
             ]
         );
+    }
+
+    protected onNavClick(): void {
+        this.showNavBar.update((showNavBar: boolean) => showNavBar = !showNavBar);
     }
 }
