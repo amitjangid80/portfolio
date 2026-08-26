f4005edec196eb264fbd56ea610f20325723143f
Amit <amitsj@fss.co.in>
2026-08-26 21:58:10 +0530

    feat: added with hash location

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,14 @@
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withInMemoryScrolling, withComponentInputBinding, withPreloading, PreloadAllModules } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, withComponentInputBinding, withPreloading, PreloadAllModules, withHashLocation } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideBrowserGlobalErrorListeners(),
         provideRouter(
             routes,
+            withHashLocation(),
             withPreloading(PreloadAllModules),
             withInMemoryScrolling({ scrollPositionRestoration: 'top', anchorScrolling: 'enabled' }),
             withComponentInputBinding()
