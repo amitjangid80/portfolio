127ba14132c6e7cfffd1ad5c1107fdbd7076532c
Amit <amitsj@fss.co.in>
2026-08-26 12:35:48 +0530

    feat: updated security headers

--- cloudflare/security-headers-worker.js
+++ cloudflare/security-headers-worker.js
@@ -5,7 +5,7 @@
 
 const SECURITY_HEADERS = {
 	"Content-Security-Policy":
-		"default-src 'self'; frame-src 'none'; media-src 'none'; child-src 'none'; base-uri 'self'; object-src 'none'; frame-ancestors 'none'; script-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com data:; img-src 'self' data: blob:; connect-src 'self'; form-action 'self'; manifest-src 'self'; worker-src 'self' blob:; require-trusted-types-for 'script'; trusted-types angular angular#bundler angular#components angular#unsafe-bypass; upgrade-insecure-requests",
+		"default-src 'self'; frame-src 'none'; media-src 'none'; child-src 'none'; base-uri 'self'; object-src 'none'; frame-ancestors 'none'; script-src 'self' https://static.cloudflareinsights.com/beacon.min.js; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com data:; img-src 'self' data: blob:; connect-src 'self'; form-action 'self'; manifest-src 'self'; worker-src 'self' blob:; require-trusted-types-for 'script'; trusted-types angular angular#bundler angular#components angular#unsafe-bypass; upgrade-insecure-requests",
 	"Cross-Origin-Embedder-Policy": "require-corp",
 	"Cross-Origin-Opener-Policy": "same-origin",
 	"Cross-Origin-Resource-Policy": "same-origin",
@@ -18,7 +18,6 @@ const SECURITY_HEADERS = {
 	"X-DNS-Prefetch-Control": "off",
 	"X-Frame-Options": "DENY",
 	"X-Permitted-Cross-Domain-Policies": "none",
-	"X-Robots-Tag": "noindex, nofollow",
 };
 
 export default {
